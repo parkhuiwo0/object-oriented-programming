28e5602b1f56360235e7bc5e40abe50a114ea0ec
HuiwooPark <huiwo0@naver.com>
2020-07-12 15:13:49 +0900

    GRASP의 CREATOR Pattern 적용

--- src/main/java/domain/Screening.java
+++ src/main/java/domain/Screening.java
@@ -4,14 +4,25 @@ import java.time.LocalDateTime;
 
 public class Screening {
 
-    private Movie move;
+    private Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
 
-    public Screening(Movie move, int sequence, LocalDateTime whenScreened) {
-        this.move = move;
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
         this.sequence = sequence;
         this.whenScreened = whenScreened;
+    }
+
+    /*
+    GRASP의 Creator Pattern
+     */
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
     }
 
     public LocalDateTime getStartTime() {
