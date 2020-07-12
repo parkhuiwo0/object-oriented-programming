7f3206ce38c6aa10846880b92d99a022ba7d3152
HuiwooPark <huiwo0@naver.com>
2020-07-12 16:31:03 +0900

    응집도를 높이기 위한 클래스 분리

--- src/main/java/domain/PeriodCondition.java
+++ src/main/java/domain/PeriodCondition.java
@@ -3,6 +3,12 @@ package domain;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
+/**
+ * 클래스의 응집도를 높이기 위해,
+ * 1) 초기화 되는 속성의 그룹을 기준으로
+ * 2) 클래스의 변경 이유가 하나로 제한되도록
+ * 할인 조건에 대한 분리를 진행
+ */
 public class PeriodCondition implements DiscountCondition {
     private DayOfWeek dayOfWeek;
     private LocalTime startTime;
