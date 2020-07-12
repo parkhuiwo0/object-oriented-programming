b86e4b3c041deecd89cb8ee8c31f64277f71965e
HuiwooPark <huiwo0@naver.com>
2020-07-12 17:19:21 +0900

    Movie 클래스 다형성 패턴 및 변경보호 패턴 적용

--- src/main/java/domain/Movie.java
+++ src/main/java/domain/Movie.java
@@ -1,24 +1,27 @@
 package domain;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
-public class Movie {
+public abstract class Movie {
 
     private String title;
     private Duration runningTime;
     private Money fee;
-    private DiscountPolicy discountPolicy;
+    private List<DiscountCondition> discountConditions;
 
-    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition ...discountConditions) {
         this.title = title;
         this.runningTime = runningTime;
         this.fee = fee;
-        this.discountPolicy = discountPolicy;
+        this.discountConditions = Arrays.asList(discountConditions);
     }
 
     public Money getFee() {
         return fee;
     }
+
     /*
     두 가지의 할인 종류 정책이 존재하는데, 코드 상에서는 어떤 할인 정책을 사용할 것인지에 대해 결정하지 않는다.
     단지 discountPolicy에 메시지를 전송할 뿐이다.
@@ -26,8 +29,17 @@ public class Movie {
     상속(inheritance), 다형성, 그리고 추상화(abstraction)
      */
     public Money calculateMovieFee(Screening screening) {
-        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+        if (isDiscoiuntable(screening)) {
+            return fee.minus(calculateDiscountAmount());
+        }
     }
+
+    private boolean isDiscountable(Screening screening) {
+        return discountConditions.stream()
+                .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    abstract protected Money calculateDiscountAmount();
 
     /*
     실행 시점에 할인 정책을 변경할 수 있도록 하기 위해서, (유연한 설계의 목표)
