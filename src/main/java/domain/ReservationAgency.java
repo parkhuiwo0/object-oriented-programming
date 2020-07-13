0fa72d05b211129662cab068e87ea9789923496f
HuiwooPark <huiwo0@naver.com>
2020-07-13 22:20:05 +0900

    ReservationAgency 클래스 응집도 높은 메서드로 분할

---
+++ src/main/java/domain/ReservationAgency.java
@@ -0,0 +1,42 @@
+package domain;
+
+public class ReservationAgency {
+
+    public Reservation reserve(Screening screening, Customor customor, int audienceCount) {
+        boolean discountable = checkDiscountable(screening);
+    }
+
+    private boolean checkDiscountable(Screening screening) {
+        return screening.getMovie().getDiscountConditions().stream()
+                .anyMatch(condition -> isDiscountable(condition, screening));
+    }
+
+    private boolean isDiscountable(DiscountCondition condition, Screening screening) {
+        if (condition.getType() == DiscountConditionType.PERIOD) {
+            return isSatisfiedByPeriod(condition, screening);
+        }
+
+        return isSatisfiedBySequence(condition, screening);
+    }
+
+    private boolean isSatisfiedBySequence(DiscountCondition condition, Screening screening) {
+        return condition.getSequence() == screening.getSequence();
+    }
+
+    private boolean isSatisfiedByPeriod(DiscountCondition condition, Screening screening) {
+        return screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
+                condition.getStartTime().compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
+                condition.getEndTime().compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
+    }
+
+    private Money calculateFee(Screening screening, boolean discountable, int audienceCount) {
+        if (discountable) {
+            return screening.getMovie().getFee()
+                    .minus(calculateDiscountedFee(screening.getMovie()))
+                    .times(audienceCount);
+        }
+
+        return screening.getMovie().getFee().times(audienceCount);
+    }
+
+}
