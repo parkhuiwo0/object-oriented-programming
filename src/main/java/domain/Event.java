b8e52615a787bc23008cfa76f557ba52c328e312
HuiwooPark <huiwo0@naver.com>
2020-07-19 19:02:55 +0900

    명령-쿼리 분리 원칙 : 명령과 쿼리 메소드 분리하기

--- src/main/java/domain/Event.java
+++ src/main/java/domain/Event.java
@@ -15,18 +15,35 @@ public class Event {
         this.duration = duration;
     }
 
+//    public boolean isSatisfied(RecurringSchedule schedule) {
+//        if (from.getDayOfWeek() != schedule.getDayOfWeek() ||
+//                !from.toLocalTime().equals(schedule.getFrom()) ||
+//                !duration.equals(schedule.getDuration())) {
+//            reschedule(schedule);
+//            return false;
+//        }
+//
+//        return true;
+//    }
+
     public boolean isSatisfied(RecurringSchedule schedule) {
         if (from.getDayOfWeek() != schedule.getDayOfWeek() ||
                 !from.toLocalTime().equals(schedule.getFrom()) ||
                 !duration.equals(schedule.getDuration())) {
-            reschedule(schedule);
             return false;
         }
 
         return true;
     }
 
-    private void reschedule(RecurringSchedule schedule) {
+//    private void reschedule(RecurringSchedule schedule) {
+//        from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)),
+//                schedule.getFrom());
+//
+//        duration = schedule.getDuration();
+//    }
+
+    public void reschedule(RecurringSchedule schedule) {
         from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)),
                 schedule.getFrom());
 
